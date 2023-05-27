c10cf58cc8d2a899b769c6d936524ec71aa11502
aflopez314 <35818794+aflopez314@users.noreply.github.com>
2023-05-27 15:02:40 -0600

    updated BAT file
    File needed updating to make the general installation easier for users.

--- Project MDIB/start_app.bat
+++ Project MDIB/start_app.bat
@@ -1,4 +1,25 @@
 ECHO Starting Python Application
-cd /d C:
-CALL python %0\..\unemploymentUpdate.py
+
+:: Create Environment
+py -3 -m venv .venv
+
+:: Activate Environment
+CALL .venv\Scripts\activate
+
+:: Install flask and other modules required for the application
+pip install Flask
+pip install Jinja2
+pip install pandas
+pip install sqlite3
+
+
+:: Set flask application
+set FLASK_APP=unemploymentUpdate.py
+set FLASK_ENV=development
+
+:: Start application and open to localhost webpage
+start "chrome.exe" http://127.0.0.1:5000/
+flask run
+
+
 PAUSE
